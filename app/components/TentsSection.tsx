c47cac71fbb240c098b19daa5138fbf87e8df70f
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-19 19:49:39 +0200

    save

---
+++ app/components/TentsSection.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+interface TentsSectionProps {
+  imageSrc: string;
+}
+
+const TentsSection: React.FC<TentsSectionProps> = ({ imageSrc }) => {
+  return (
+    <article className="relative flex flex-col min-h-[400px] min-w-[240px] transform transition-transform duration-300 ease-in-out hover:scale-105 p-5 m-10">
+      <Image
+        src={imageSrc}
+        alt={"image"}
+        fill
+        loading="lazy"
+        className="object-cover rounded-3xl"
+        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
+      />
+    </article>
+  );
+};
+
+export default TentsSection;
