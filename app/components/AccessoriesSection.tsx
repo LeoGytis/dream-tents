f90cc2c6421212d1b0781848c3a09e81b9ed0071
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-26 21:03:36 +0200

    save

--- app/components/AccessoriesSection.tsx
+++ app/components/AccessoriesSection.tsx
@@ -1,19 +1,104 @@
+"use client";
+import { useState } from "react";
+import { IconType } from "react-icons";
+import { GiEmptyChessboard, GiWoodenChair } from "react-icons/gi";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { MdOutlineTableRestaurant } from "react-icons/md";
+import { PiDiscoBallFill } from "react-icons/pi";
+import { SiBlockbench } from "react-icons/si";
+
+interface IconWithLabelProps {
+  Icon: IconType;
+  label: string;
+  onClick: () => void;
+}
+
+const IconWithLabel: React.FC<IconWithLabelProps> = ({
+  Icon,
+  label,
+  onClick,
+}) => {
+  return (
+    <div
+      className="flex flex-col items-center gap-2 hover:cursor-pointer"
+      onClick={onClick}
+    >
+      <Icon className="w-12 h-12 text-orange-700" />
+      <span>{label}</span>
+    </div>
+  );
+};
+
 const AccessoriesSection = () => {
+  const [selectedIcon, setSelectedIcon] = useState<string | null>(null);
+
+  const handleIconClick = (label: string) => {
+    setSelectedIcon(label);
+  };
+
+  const getDescription = (label: string) => {
+    switch (label) {
+      case "Chairs":
+        return "Comfortable and stylish chairs for any event.";
+      case "Chessboard":
+        return "High-quality chessboards for strategic fun.";
+      case "Lighting":
+        return "Ambient lighting solutions to create the perfect atmosphere.";
+      case "Disco Ball":
+        return "A shining disco ball to light up the dance floor.";
+      case "Bench":
+        return "Elegant benches for seating at your event.";
+      case "Tables":
+        return "Versatile tables that suit any event's needs.";
+      default:
+        return "";
+    }
+  };
+
   return (
     <section
       id="accessoriesSection"
-      className="w-full h-[400px] relative flex p-5 m-10"
+      className="w-full h-[400px] flex flex-col  gap-4 my-10 mx-10"
     >
-      <div className="w-[100vw] h-[400px] absolute left-1/2 transform -translate-x-1/2 flex">
-        <div className="w-1/2 flex flex-col justify-center items-end gap-4 p-6">
-          <h1 className="">Accessories</h1>
-          <span className="">Tables</span>
-          <span className="">Chairs</span>
-          <span className="">Floor</span>
-          <span className="">Globe</span>
-          <span className="">Bench</span>
-        </div>
+      <div className="flex justify-between items-center border-b-2 pb-6">
+        <IconWithLabel
+          Icon={GiWoodenChair}
+          label="Chairs"
+          onClick={() => handleIconClick("Chairs")}
+        />
+        <IconWithLabel
+          Icon={GiEmptyChessboard}
+          label="Chessboard"
+          onClick={() => handleIconClick("Chessboard")}
+        />
+        <IconWithLabel
+          Icon={HiOutlineLightBulb}
+          label="Lighting"
+          onClick={() => handleIconClick("Lighting")}
+        />
+        <IconWithLabel
+          Icon={PiDiscoBallFill}
+          label="Disco Ball"
+          onClick={() => handleIconClick("Disco Ball")}
+        />
+        <IconWithLabel
+          Icon={SiBlockbench}
+          label="Bench"
+          onClick={() => handleIconClick("Bench")}
+        />
+        <IconWithLabel
+          Icon={MdOutlineTableRestaurant}
+          label="Tables"
+          onClick={() => handleIconClick("Tables")}
+        />
       </div>
+
+      {selectedIcon && (
+        <div className="mt-6 text-center">
+          <h2>{selectedIcon}</h2>
+          <p>{getDescription(selectedIcon)}</p>
+        </div>
+      )}
     </section>
   );
 };
