817c3ce492080215360f170f08b20a8cc3ece0df
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-26 17:48:59 +0200

    save

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -8,7 +8,7 @@ interface NavLinkProps {
 
 const NavLink: React.FC<NavLinkProps> = ({ href, children }) => {
   return (
-    <div className="rounded py-2 px-4 cursor-pointer group border border-transparent hover:bg-orange-100 hover:border-orange-300 transition-colors duration-300 text-xl">
+    <div className="rounded cursor-pointer group border-b border-b-transparent hover:bg-orange-100 hover:border-b-orange-300 transition-colors duration-300 text-xl py-2 px-6">
       <Link href={href}>{children}</Link>
     </div>
   );
@@ -16,14 +16,19 @@ const NavLink: React.FC<NavLinkProps> = ({ href, children }) => {
 
 export const Navbar = () => {
   return (
-    <section className="mx-auto max-w-screen-xl sticky top-0 z-50 w-full flex flex-col items-center gap-4 pt-4 nav-bar">
-      <Image src="/images/logo.jpg" alt="logo" width={150} height={150} />
-      <nav className="w-full flex justify-between items-center gap-4 border-b border-orange-400 pb-4 px-40 ">
+    <section className="sticky top-0 z-50 flex justify-between items-center border-b border-orange-400 py-4 px-10 nav-bar">
+      <Image
+        src="/images/logo_orange.png"
+        alt="logo"
+        width={150}
+        height={150}
+      />
+      <nav className="w-full flex justify-end items-center gap-2">
         <NavLink href="/tents">Tents</NavLink>
+        <NavLink href="/prices">Prices</NavLink>
+        <NavLink href="/galery">Gallery</NavLink>
         <NavLink href="/about">About Us</NavLink>
         <NavLink href="/contact">Contact</NavLink>
-        <NavLink href="/prices">Prices</NavLink>
-        <NavLink href="/galery">Gallerie</NavLink>
       </nav>
     </section>
   );
