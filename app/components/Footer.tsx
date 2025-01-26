f3204c058a07bc872ddfaecd9a4565541cd82d10
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-26 22:46:32 +0200

    save

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,9 @@
+"use client";
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <section className="bg-[#151515] border-t-2 border-[#FF5421] text-white footer">
+    <section className="bg-[#151515] border-t-2 border-[#FF5421] text-white mt-10">
       <div className="mx-auto max-w-screen-xl flex justify-between items-center text-lg py-16 ">
         <div className="flex flex-col gap-4">
           <h3>Paslaugos</h3>
@@ -14,11 +17,22 @@ const Footer = () => {
           <span>Swedbank</span>
           <span>A/S: LTLT157300010158225022</span>
         </div>
-        <div className="flex flex-col gap-4">
-          <h3>Kontaktai</h3>
-          <span>+370 682 12888</span>
-          <span>info@dreamtents.lt</span>
-          <span>Logo here</span>
+        <div className="flex flex-col gap-10">
+          <Image
+            src="/images/logo_orange.png"
+            alt="logo"
+            width={80}
+            height={80}
+            className="hover:cursor-pointer self-center"
+          />
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <a href="tel:+37068212888" className="hover:underline ">
+              +370 682 12888
+            </a>
+            <a href="mailto:info@dreamtents.lt" className="hover:underline">
+              info@dreamtents.lt
+            </a>
+          </div>
         </div>
       </div>
     </section>
