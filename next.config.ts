86275b82ab5351e7c1eb15f3d8d71c7ef60646b1
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-05-28 11:28:35 +0300

    save

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-	output: 'export',
+	// output: 'export',
+	// images: {unoptimized: true},
+	basePath: isProd ? '/dream-tents' : '',
+	assetPrefix: isProd ? '/dream-tents/' : '',
 	images: {unoptimized: true},
 };
 
